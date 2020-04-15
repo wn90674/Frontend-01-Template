19b00697810bea0060f5c9881b8ec18c0c6ed63f
wn90674 <maple0421@163.com>
2020-04-15 21:17:39 +0800

    test

---
+++ week01/url解析.js
@@ -0,0 +1,19 @@
+/**
+ * url 解析
+ * @param search window.location.search
+ * @returns Object
+ */
+function urlSearch(search) {
+  let result = {};
+
+  const queryString = decodeURIComponent(search.slice(1))
+  const queryStringList = queryString.split(/=|&/);
+  const length = queryStringList.length;
+  for (let i = 0; i < length - 1; i += 2) {
+      const key = queryStringList[i];
+      const value = queryStringList[i + 1];
+      result[key] = value;
+  }
+
+  return result;
+}
